d39ceb15bf88b909b27be1cd8dfb161ab8154449
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-07-24 14:35:33 +0200

    [add server.hpp]

--- inc/server.hpp
+++ inc/server.hpp
@@ -3,14 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adegain <adegain@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sleon <sleon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 13:39:36 by adegain           #+#    #+#             */
-/*   Updated: 2023/07/24 13:39:59 by adegain          ###   ########.fr       */
+/*   Updated: 2023/07/24 14:31:13 by sleon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_HPP
-#define SERVER_HPP
+# define SERVER_HPP
+
+# include <string>
+# include <list>
+
+class server
+{
+	private:
+		const std::string _serverName;
+		const std::string _password;
+		server();
+		~server();
+	public:
+		void	start(const int port, const std::string password);
+		void	acceptConnexions();
+		void	manageClientMsg();
+		void	executeCommand();
+		void	createChannel();
+		void	delChannel();
+		void	doesChannelExist();
+		void	getClientByFd();
+		void	getClientByName();
+		void	addClient();
+		void	removeClient();
+		void	getServerName();
+		void	getServerAddress();
+		void	getPort();
+};
+
+server::server()
+{
+}
+
+server::~server()
+{
+}
+
 
 #endif
