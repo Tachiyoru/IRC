124acb60e1468e5fa3758fc548f3fd5c3a42782a
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-08-15 17:50:03 +0200

    [add serv is socket ready]

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,32 +6,109 @@
 /*   By: sleon <sleon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 15:55:30 by sleon             #+#    #+#             */
-/*   Updated: 2023/07/25 12:51:36 by sleon            ###   ########.fr       */
+/*   Updated: 2023/08/15 17:47:16 by sleon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Server.hpp"
+#include "Server.hpp"
+#include <stdio.h>
 
 // ************************************************************************** //
-//                                static-private                              //
+//                        Constructors / Destructors                          //
 // ************************************************************************** //
 
-// std::map<std::string const, Server::fct const> const	Server::_commandList[] = {
-// 	std::map<Server::fct const>(std::string("x"), &Server::x),
-// 	std::map<Server::fct const>(std::string(), NULL)
-// };
+Server::Server() : _serverName("Anne.Shan"), _password(), _socket(-1), _state(1),
+	_pollfds(){}
+
+Server::~Server() {}
 
 // ************************************************************************** //
-//                                Constructors                                //
+//                              Public functions                              //
 // ************************************************************************** //
 
-// Server::Server() : _serverName("AnneShan"), , _clients(),
-// 	{}
+bool	Server::run()
+{
+	this->_state = 0;
+	// std::cout<<"normalement ca tourne"<<std::endl;
+	while (!this->_state)
+	{
+		if (!this->_state || !this->welcoming())
+		{
+			this->stop();
+			return 1;
+		}
+	}
+	return 0;
+}
 
-// Server::~Server() {}
+void	Server::stop()
+{
+	std::cout<<"Server shutdown initiation"<<std::endl;
+	if (this->_socket != -1)
+		close(this->_socket);
+	this->_socket = 1;
+	this->_state = 1;
+	// this->_channelList.clear();
+	// this->_clientList.clear();
+	// this->_commandList.clear();
+}
 
+bool	Server::init(const int port, const std::string password)
+{
+	sockaddr_in addr;
 
+	this->_password = password;
+	this->_port = port;
+	this->_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (this->_socket < 0)
+		return (std::cerr<<"Error: socket: creation "<<std::endl, 1);
+	int flags = fcntl(_socket, F_GETFL, 0); //socket non bloquante je sais pas encore si c'est pertinant ou pas
+	if (flags < 0)
+	{
+		this->stop();
+		return (std::cerr<<"Error: socket: fcntl(F_GETFL) call"<<std::endl, 1);
+	}
+	if (fcntl(_socket, F_SETFL, flags | O_NONBLOCK) < 0)
+	{
+		this->stop();
+		return (std::cerr<<"Error: socket: fcntl(F_SETFL) call"<<std::endl, 1);
+	}
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(_port);
+	addr.sin_family = AF_INET;
+	if (bind(this->_socket, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)))
+	{
+		this->stop();
+		return (std::cerr<<"Error: socket: bind call"<<std::endl, 1);
+	}
+	if (listen(this->_socket, SOMAXCONN))
+	{
+		this->stop();
+		return (std::cerr<<"Error: socket: listen call"<<std::endl, 1);
+	}
+	pollfd pollfd_1;
+	pollfd_1.fd = this->_socket;
+	pollfd_1.events = POLLIN | POLLOUT;
+	_pollfds.push_back(pollfd_1);
+	// _commandList["PASS"] = &fct_PASS;
+	// _commandList["NICK"] = &fct_NICK;
+	// _commandList["USER"] = &fct_USER;
+	// _commandList["MOTD"] = &fct_MOTD;
+	// _commandList["TIME"] = &fct_TIME;
+	// _commandList["PING"] = &fct_PING;
+	// _commandList["QUIT"] = &fct_QUIT;
+	// _commandList["TOPIC"] = &fct_TOPIC;
+	// _commandList["NAMES"] = &fct_NAMES;
+	// _commandList["JOIN"] = &fct_JOIN;
+	// _commandList["PART"] = &fct_PART;
+	// _commandList["MODE"] = &fct_MODE;
+	// _commandList["OPER"] = &fct_OPER;
+	// _commandList["KILL"] = &fct_KILL;
+	// _commandList["KICK"] = &fct_KICK;
+	// _commandList["PRIVMSG"] = &fct_PRIVMSG;
+	// _commandList["NOTICE"] = &fct_NOTICE;
+	// _commandList["INVITE"] = &fct_INVITE;
+	// _commandList["LIST"] = &fct_LIST;
+	return 0;
+}
 
-// bool	init(const int port, const std::string password){
-
-// }
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,37 +6,52 @@
 /*   By: sleon <sleon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 13:39:25 by adegain           #+#    #+#             */
-/*   Updated: 2023/07/25 13:04:35 by sleon            ###   ########.fr       */
+/*   Updated: 2023/08/15 16:41:35 by sleon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 #include <stdlib.h>
+#include <signal.h>
 
-bool	checkPort(std::string str, int value){
+bool	checkPort(std::string str, int value)
+{
 	std::string::const_iterator	it;
 
-	for (it = str.begin(); it != str.end(); ++it){
-		if (!isdigit(*it)){
+	for (it = str.begin(); it != str.end(); ++it)
+	{
+		if (!isdigit(*it))
+		{
 			std::cerr<<"error: port: nondigit value"<<std::endl;
 			return false;
 		}
 	}
-	if ((value >= 0 && value < 1024) || value > 65535){
+	if ((value >= 0 && value < 1024) || value > 65535)
+	{
 		std::cerr<<"error: port: out of range"<<std::endl;
 		return false;
 	}
 	return true;
 }
 
-int	main(int argc, char** argv){
+void	sigHandler(int sig)
+{
+	if (sig == SIGINT){
+		// server.stop();
+		exit(4);
+	}
+}
 
-	// Server		server;
+int	main(int argc, char** argv)
+{
+	Server		server;
 
 	if (argc != 3)
 		return(std::cerr<<"Irc should run as : ./ircserv <port> <password>"<<std::endl, 1);
 	if (!checkPort(argv[1], atoi(argv[1])))
 		return 2;
-	// if (!server.init(atoi(argv[1]), argv[2]))
-	// 	return 3;
+	signal(SIGINT, sigHandler);
+	if (server.init(atoi(argv[1]), argv[2]) || server.run())
+		return 3;
+
 }
