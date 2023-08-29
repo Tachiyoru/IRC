f770e1666dce88fffa3a501e8c119c6d614efc53
adegain <adegain@student.42.fr>
2023-08-29 17:20:16 +0200

    [ADD] invite + list cmds

---
+++ cmds/INVITE.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   INVITE.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adegain <adegain@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 15:52:24 by adegain           #+#    #+#             */
+/*   Updated: 2023/08/29 15:52:44 by adegain          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/RFC1459.hpp"
+#include "../inc/Server.hpp"
+#include "../inc/Channel.hpp"
+#include "../inc/Client.hpp"
+#include "../inc/Utils.hpp"
+
+void	RFC1459_INVITE(Client* sender, parsedCmd_t& pc)
+{
+	if (!ensureAuth(sender))
+	{
+		RFC1459_ERR_NOTREGISTERED(sender);
+		return;
+	} 
+	REGISTER_COMMAND("INVITE", sender);
+	Client*			cli;
+	Channel*		chan;
+	channelMode_t*	cm;
+	
+	if (!pc.has_words || pc.words.size() != 2)
+		RFC1459_ERR_NEEDMOREPARAMS(sender, cmd_name);
+	else
+	{
+		cli = server->findClient(pc.words[0]);
+		chan = server->findChannel(pc.words[1]);
+		cm = chan->getMode();
+		if (!cli)
+			RFC1459_ERR_NOSUCHNICK(sender, pc.words[0]);
+		else if (!chan)
+			return;
+		else if (cm->i && (!chan->isOperator(sender) && !chan->isVoiced(sender)))
+			RFC1459_ERR_CHANOPRIVSNEEDED(sender, pc.words[1]);
+		else if (!cm->i && !chan->isConnected(sender))
+			RFC1459_ERR_NOTONCHANNEL(sender, pc.words[1]);
+		else if (chan->isConnected(cli))
+		{
+			args.push_back(pc.words[0]);
+			args.push_back(pc.words[1]);
+			server->sendString(sender, format(ERR_USERONCHANNEL, args));
+		}
+		else
+		{
+			args.push_back(pc.words[0]);
+			args.push_back(pc.words[1]);
+			server->sendString(sender, format(RPL_INVITING, args));
+			args.clear();
+			args.push_back(sender->getNick());
+			args.push_back(sender->getUser());
+			args.push_back(sender->getHost());
+			args.push_back(pc.words[0]);
+			args.push_back(pc.words[1]);
+			server->sendString(cli, format(RPL_INVITE, args));
+			chan->addInvite(cli);
+		}
+	}
+}
---
+++ cmds/LIST.cpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   LIST.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adegain <adegain@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 15:51:58 by adegain           #+#    #+#             */
+/*   Updated: 2023/08/29 15:51:59 by adegain          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../inc/RFC1459.hpp"
+#include "../inc/Server.hpp"
+#include "../inc/Channel.hpp"
+#include "../inc/Client.hpp"
+#include "../inc/Utils.hpp"
+
+void	RFC1459_LIST(Client* sender, parsedCmd_t& pc)
+{
+	if (!ensureAuth(sender))
+	{
+		RFC1459_ERR_NOTREGISTERED(sender);
+		return;
+	}
+	REGISTER_COMMAND("LIST", sender);
+	vector<string>					canals;
+	size_t							i;
+	map<string, Channel*>			*channels;
+	map<string, Channel*>::iterator	it;
+	channelMode_t*					cm;
+	Channel*						chan;
+
+	server->sendString(sender, format(RPL_LISTSTART, args));
+	if (!pc.has_words)
+	{
+		channels = server->getChannels();
+		for (it = channels->begin(); it != channels->end(); it++)
+		{
+			cm = it->second->getMode();
+			REFRESH_REPLY(sender);
+			if (it->second->isConnected(sender) || (!cm->p && !cm->s))
+			{
+				args.push_back(it->second->getName());
+				args.push_back(it->second->getVisibleCount());
+				args.push_back(it->second->getTopic());
+			}
+			else if (cm->p)
+			{
+				args.push_back("Prv");
+				args.push_back(it->second->getVisibleCount());
+				args.push_back("");
+			}
+			else
+				continue;
+			server->sendString(sender, format(RPL_LIST, args));
+		}
+	}
+	else
+	{
+		canals = split(pc.words[0], ",");
+		for (i = 0; i < canals.size(); i++)
+		{
+			chan = server->findChannel(canals[i]);
+			if (!chan)
+				continue;
+			cm = chan->getMode();
+			REFRESH_REPLY(sender);
+			if (chan->isConnected(sender) || (!cm->p && !cm->s))
+			{
+				args.push_back(it->second->getName());
+				args.push_back(it->second->getVisibleCount());
+				args.push_back(it->second->getTopic());
+			}
+			else if (cm->p)
+			{
+				args.push_back("Prv");
+				args.push_back(it->second->getVisibleCount());
+				args.push_back("");
+			}
+			else
+				continue;
+			server->sendString(sender, format(RPL_LIST, args));
+		}
+	}
+	REFRESH_REPLY(sender);
+	server->sendString(sender, format(RPL_LISTEND, args));
+}
