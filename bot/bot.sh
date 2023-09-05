dac7e563c4d675850cf4bfcb51db21fdee51b265
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-09-05 14:47:28 +0200

    [added RPS Bot]

---
+++ bot/bot.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+input=".botchatlogs"
+
+HOST="$1"
+PORT="$2"
+PASS="$3"
+NAME="Bot"
+CHAN="#RockPaperScissors"
+
+if [ "$#" -ne 3]; then
+	echo "Please launche the bot by using : 'sh $0 <host> <port> <password>'"
+	exit 1
+fi
+
+log_n_join()
+{
+	printf "PASS ${PASS}\r\n"
+	printf "NICK ${NAME}\r\n"
+	printf "USER ${NAME} * * :${NAME}\r\n"
+	printf "JOIN ${CHAN}\r\n"
+}
+
+log_n_join > $input
+trap "rm -f $input;exit 0" INT TERM EXIT
+tail -f $input | nc $HOST $PORT | while read line; do
+	if (echo "$line" | grep "PRIVMSG ${CHAN}"); then
+		RANDOM = $(shuf -i 0-999 -n1)
+		rps = $((1 + RANDOM % 3))
+		if ["$rps" -eq 1]; then
+			printf "NOTICE ${CHAN} :ROCK\r\n" >> $input
+			if (echo "$line" | grep "ROCK" >> /dev/null); then
+				printf "NOTICE ${CHAN} :It's a tie\r\n" >> $input
+			elif (echo "$line" | grep "PAPER" >> /dev/null); then
+				printf "NOTICE ${CHAN} :You lose\r\n" >> $input
+			elif (echo "$line" | grep "SCISSORS" >> /dev/null); then
+				printf "NOTICE ${CHAN} :Nice you won\r\n" >> $input
+			else
+				printf "NOTICE ${CHAN} :Do you really know the rules ?\r\n" >> $input
+			fi
+		if ["$rps" -eq 2]; then
+			printf "NOTICE ${CHAN} :PAPER\r\n" >> $input
+			if (echo "$line" | grep "ROCK" >> /dev/null); then
+				printf "NOTICE ${CHAN} :Nice you won\r\n" >> $input
+			elif (echo "$line" | grep "PAPER" >> /dev/null); then
+				printf "NOTICE ${CHAN} :It's a tie\r\n" >> $input
+			elif (echo "$line" | grep "SCISSORS" >> /dev/null); then
+				printf "NOTICE ${CHAN} :You lose\r\n" >> $input
+			else
+				printf "NOTICE ${CHAN} :Do you really know the rules ?\r\n" >> $input
+			fi
+		if ["$rps" -eq 1]; then
+			printf "NOTICE ${CHAN} :SCISSORS\r\n" >> $input
+			if (echo "$line" | grep "ROCK" >> /dev/null); then
+				printf "NOTICE ${CHAN} :You lose\r\n" >> $input
+			elif (echo "$line" | grep "PAPER" >> /dev/null); then
+				printf "NOTICE ${CHAN} :Nice you won\r\n" >> $input
+			elif (echo "$line" | grep "SCISSORS" >> /dev/null); then
+				printf "NOTICE ${CHAN} :It's a tie\r\n" >> $input
+			else
+				printf "NOTICE ${CHAN} :Do you really know the rules ?\r\n" >> $input
+			fi
+		fi
+	fi
+done
